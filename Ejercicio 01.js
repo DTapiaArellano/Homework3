86497a6f48d4f39d348cc0db60b5b23b55632315
DTapiaArellano <dtarellano21@gmail.com>
2023-04-10 18:06:50 -0400

    Homework3

---
+++ Ejercicio 01.js
@@ -0,0 +1,36 @@
+/*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
+/*1️⃣ EJERCICIO 01 1️⃣*/
+
+// En los siguientes ejercicios deberás reemplazar el valor null
+// por el correspondiente.
+
+// Crea una variable de tipo string.
+const nuevoString = "String";
+
+// Crea una variable de tipo number.
+const nuevoNumero = 15;
+
+// Crea una variable de tipo boolean.
+const nuevoBoolean = true;
+
+// Resuelve el siguiente problema matemático.
+const nuevaResta = 10 - null === 3;
+nuevaResta = 7;
+
+// Resuelve el siguiente problema matemático.
+const nuevaMultiplicacion = 10 * null === 40;
+nuevaMultiplicacion = 400;
+
+// Resuelve el siguiente problema matemático.
+const nuevoModulo = 21 % 5 === null;
+nuevoModulo = 1;
+
+/*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
+module.exports = {
+   nuevoString,
+   nuevoNumero,
+   nuevoBoolean,
+   nuevaResta,
+   nuevaMultiplicacion,
+   nuevoModulo,
+};
